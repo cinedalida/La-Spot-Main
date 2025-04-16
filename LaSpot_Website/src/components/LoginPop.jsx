3ca4a4d572a7533d9bc5f4e664a42bbb7fcd9502
Francine Dalida <francine.dalida@gmail.com>
2025-04-17 01:03:15 +0800

    Added: Forgot Password

--- LaSpot_Website/src/components/LoginPop.jsx
+++ LaSpot_Website/src/components/LoginPop.jsx
@@ -1,10 +1,12 @@
 import "../css/LoginPop.css";
 import React, { useState, useRef } from "react";
+import ForgotPassword from "./ForgotPassword";
 
 const LoginPop = ({ setIsLoginOpen }) => {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const [errors, setErrors] = useState({ email: false, password: false });
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
 
   // Login Form Validation
   const handleSubmit = (event) => {
@@ -41,57 +43,69 @@ const LoginPop = ({ setIsLoginOpen }) => {
 
   return (
     <div className="login-overlay">
-      <div className="login-container">
-        {/* Left side: Image */}
-        <div className="login-image">
-          <img src="/images/LoginImage.png" alt="Parking" />
-        </div>
-
-        {/* Right side: Login Form */}
-        <div className="login-form">
-          {/* Close button */}
-          <button className="close-btn" onClick={() => setIsLoginOpen(false)}>
-            ✖
-          </button>
-
-          {/* Logo and Title */}
-          <div className="logo-container">
-            <img
-              src="/images/LaspotLogo.png"
-              alt="logo"
-              className="login-logo"
-            />
-            <h1 className="logo-title">La Spot</h1>
+      {/* Forgot Password */}
+      {isForgotPassword ? (
+        <ForgotPassword goBackToLogin={() => setIsForgotPassword(false)} />
+      ) : (
+        <div className="login-container">
+          {/* Left side: Image */}
+          <div className="login-image">
+            <img src="/images/LoginImage.png" alt="Parking" />
           </div>
 
-          <h2 className="login-title">Login</h2>
-          <p className="login-description">
-            Enter your email and password to login
-          </p>
-
-          {/* Login Form */}
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              placeholder="Email"
-              className={`inputField-login ${errors.email ? "error" : ""}`}
-              ref={emailRef}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className={`inputField-login ${errors.password ? "error" : ""}`}
-              ref={passwordRef}
-            />
-            <a href="#" className="forgot-password">
-              Forgot Password?
-            </a>
-            <button type="submit" className="login-button">
-              Login
+          {/* Right side: Login Form */}
+          <div className="login-form">
+            {/* Close button */}
+            <button className="close-btn" onClick={() => setIsLoginOpen(false)}>
+              ✖
             </button>
-          </form>
+
+            {/* Logo and Title */}
+            <div className="logo-container">
+              <img
+                src="/images/LaspotLogo.png"
+                alt="logo"
+                className="login-logo"
+              />
+              <h1 className="logo-title">La Spot</h1>
+            </div>
+
+            <h2 className="login-title">Login</h2>
+            <p className="login-description">
+              Enter your email and password to login
+            </p>
+
+            {/* Login Form */}
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                placeholder="Email"
+                className={`inputField-login ${errors.email ? "error" : ""}`}
+                ref={emailRef}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className={`inputField-login ${errors.password ? "error" : ""}`}
+                ref={passwordRef}
+              />
+              <a
+                href="#"
+                className="forgot-password"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsForgotPassword(true);
+                }}
+              >
+                Forgot Password?
+              </a>
+              <button type="submit" className="login-button">
+                Login
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
