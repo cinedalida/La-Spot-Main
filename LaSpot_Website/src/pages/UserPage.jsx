6447d9591bc80dd6a31c289f9ecd2373a8c120cd
Francine Dalida <francine.dalida@gmail.com>
2025-04-18 19:56:59 +0800

    Modified: UserParking.css and UserPage.jsx

--- LaSpot_Website/src/pages/UserPage.jsx
+++ LaSpot_Website/src/pages/UserPage.jsx
@@ -1,17 +1,24 @@
 import "../css/Header2.css";
 import "../css/Footer.css";
 import "../css/ScrollUp.css";
+import "../css/UserParking.css";
+import "../css/UserProfile.css";
 
 import { Header2 } from "../components/Header2";
 import { Footer } from "../components/Footer";
 import { ScrollUp } from "../components/ScrollUp";
+import UserParking from "../components/UserParking";
+import { UserProfile } from "../components/UserProfile";
 
 export function UserPage() {
   return (
     <>
       {/* waitings kay jan to be re-edited by cine */}
       <Header2 />
+      {/* <UserParking /> */}
+      <UserProfile />
       <Footer />
+
       <ScrollUp />
     </>
   );
