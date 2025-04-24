e8c49d2b3b9ee53881fb016bd5ff508c8740979c
Francine Dalida <francine.dalida@gmail.com>
2025-04-24 08:43:54 +0800

    Modified: App.jsx. Page at User ParkingView

--- LaSpot_Website/src/App.jsx
+++ LaSpot_Website/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       {/* <MainPage /> */}
-      <AdminPage />
-      {/* <UserPage /> */}
+      {/* <AdminPage /> */}
+      <UserPage />
       {/* <WorkerPage /> */}
     </>
   );
